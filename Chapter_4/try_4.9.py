1e92a0c581c1439a815e318f81d04f399fcba13c
charliealpha094 <carlosamaral94@gmail.com>
2020-08-02 00:02:43 +0100

    Solution to exercise try_4.9

---
+++ Chapter_4/try_4.9.py
@@ -0,0 +1,11 @@
+#Done by Carlos Amaral in 15/06/2020
+
+"""
+Use a list comprehension to generate a list of the
+first 10 cubes.
+"""
+
+
+#Cube comprehension
+cubes = [value**3 for value in range (1,11)]
+print(cubes)
