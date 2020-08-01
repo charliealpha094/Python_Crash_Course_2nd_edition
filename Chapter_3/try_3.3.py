4e4d7b1a8caa56c2f509306de661b864169d3a4e
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 18:39:23 +0100

    Solution to exercise try_3.3

--- Chapter_3/try_3.3.py
+++ Chapter_3/try_3.3.py
@@ -7,20 +7,21 @@ to print a series of statements about these items, such as “I would like to ow
 Honda motorcycle.”
 """
 
-fav_transport=['Mitsubishi', 'Suzuki', 'Toyota', 'Mercedes', 'Range Rover']
+fav_transport = ['Mitsubishi', 'Suzuki', 'Toyota', 'Mercedes', 'Range Rover']
 
-message= "I really love my " + fav_transport[0].title() + " Pajero!!"
+message = "I really love my " + fav_transport[0].title() + " Pajero!!"
 print (message)
 
-message="I would love to buy the new " +fav_transport[1].title() + " Jimny!"
+message = "I would love to buy the new " +fav_transport[1].title() + " Jimny!"
 print(message)
 
-message="I would love to have a " +fav_transport[2].title() + " Land Cruiser"
+message = "I would love to have a " +fav_transport[2].title() + " Land Cruiser"
 print(message)
 
-message= "I would love to be able to afford a "+ fav_transport[3].title() + " G!"
+message = "I would love to be able to afford a "+ fav_transport[3].title() + \
+          " G!"
 print(message)
 
-message= "I really enjoy the " + fav_transport[4].title() + " Classic!"
+message = "I really enjoy the " + fav_transport[4].title() + " Classic!"
 print(message)
 
