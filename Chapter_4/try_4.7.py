ae69cce0d0b0fa3adb79cb39b08f2e07130fa611
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 23:52:36 +0100

    Solution to exercise try_4.7

---
+++ Chapter_4/try_4.7.py
@@ -0,0 +1,10 @@
+#Done by Carlos Amaral in 15/06/2020
+
+"""
+Make a list of the multiples of 3 from 3 to 30. Use a for loop to
+print the numbers in your list.
+"""
+
+multiples_three = list(range(3,30,3))
+for number in multiples_three:
+	print(number)
