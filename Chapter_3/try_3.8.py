2a70f047517f08f7c76960b9b334a7e6d903203d
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 22:06:54 +0100

    Solution to exercise try_3.8

---
+++ Chapter_3/try_3.8.py
@@ -0,0 +1,16 @@
+Dream_places = ['Douro', 'Tokyo', 'Nassau', 'Monaco', 'Greece', 'Thailand']
+print(Dream_places)
+
+print(sorted(Dream_places))
+
+print("\nThe list of my dream places is still in alphabetical order:")
+print(Dream_places)
+
+Dream_places = ['Douro', 'Tokyo', 'Nassau', 'Monaco', 'Greece', 'Thailand']
+Dream_places.reverse()
+print(Dream_places)
+print("\nThe list is back in its original order:")
+Dream_places.reverse()
+print(Dream_places)
+Dream_places.sort(reverse=True)
+print(Dream_places)
