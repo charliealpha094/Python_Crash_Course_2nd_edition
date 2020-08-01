49492068e439c86e6139747cfbf2edff7a20ba6d
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 22:47:05 +0100

    Solution to exercise try_3.10

---
+++ Chapter_3/try_3.10.py
@@ -0,0 +1,20 @@
+cities = ['Porto', 'Lisboa', 'Viseu', 'Vigo', 'Wien']
+print(cities)
+print(len(cities))
+
+#Reverse alphabetical order
+cities.sort(reverse=True)
+print(cities)
+
+#Sorting a list temporarily with the sorted() function
+print("\nHere is the sorted list:")
+print(sorted(cities))
+
+
+#Printing a list in Reverse Order
+print(cities)
+cities.reverse()
+
+
+
+
