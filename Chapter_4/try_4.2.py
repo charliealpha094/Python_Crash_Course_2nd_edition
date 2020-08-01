ade2ca4d2f4f337c5af2d0cc4d3d0c4eee10ef6a
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 23:11:55 +0100

    Solution to exercise try_4.2

---
+++ Chapter_4/try_4.2.py
@@ -0,0 +1,13 @@
+#Done by Carlos Amaral in 15/06/2020
+
+mammals = ['dog', 'cat', 'horse', 'hamster']
+for mammal in mammals:
+	print(mammal)
+
+print("\n")
+
+for mammal in mammals:
+	print(f"A {mammal.title()} would make a great pet!")
+
+print("\n")
+print("Any of these mammals would make a great pet!!")
