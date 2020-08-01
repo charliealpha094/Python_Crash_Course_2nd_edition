ec9e89874f4e28af778d2fe3c0a49679497c75fb
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 18:15:36 +0100

    Solution to exercise try_3.3.py

---
+++ Chapter_3/try_3.3.py
@@ -0,0 +1,26 @@
+#Done by Carlos Amaral in 12/06/2020
+
+"""
+Think of your favorite mode of transportation, such as a
+motorcycle or a car, and make a list that stores several examples. Use your list
+to print a series of statements about these items, such as “I would like to own a
+Honda motorcycle.”
+"""
+
+fav_transport=['Mitsubishi', 'Suzuki', 'Toyota', 'Mercedes', 'Range Rover']
+
+message= "I really love my " + fav_transport[0].title() + " Pajero!!"
+print (message)
+
+message="I would love to buy the new " +fav_transport[1].title() + " Jimny!"
+print(message)
+
+message="I would love to have a " +fav_transport[2].title() + " Land Cruiser"
+print(message)
+
+message= "I would love to be able to afford a "+ fav_transport[3].title() + " G!"
+print(message)
+
+message= "I really enjoy the " + fav_transport[4].title() + " Classic!"
+print(message)
+
