ddf2fcd5612a9617cd4887ea0d602f948f9dcb85
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 17:38:36 +0100

    Solutions to exercise try_2.7

---
+++ try_2.7.py
@@ -0,0 +1,26 @@
+#Author: Carlos Amaral
+#Date: 11/06/2020
+
+"""
+Use a variable to represent a person’s name, and include
+some whitespace characters at the beginning and end of the name. Make sure
+you use each character combination, "\t" and "\n" , at least once.
+Print the name once, so the whitespace around the name is displayed.
+Then print the name using each of the three stripping functions, lstrip() ,
+rstrip() , and strip() .
+"""
+
+name="\tCarlos Amaral\n"
+print(name)
+
+
+print("\nUsing lstrip():")
+print (name.lstrip())
+
+
+print("\nUsing rstrip():")
+print (name.rstrip())
+
+
+print("\nUsing strip():")
+print (name.strip())
