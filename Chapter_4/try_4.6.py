4e66c7853388f204086ee6615b0a0a7d31144c5f
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 23:47:27 +0100

    Solution to exercise try_4.6

---
+++ Chapter_4/try_4.6.py
@@ -0,0 +1,16 @@
+#Done by Carlos Amaral in 15/06/2020
+
+"""
+Use the third argument of the range() function to make a
+list of the odd numbers from 1 to 20. Use a for loop to print each number.
+"""
+
+odd_numbers = list(range(1,20,2))
+print(odd_numbers)
+
+
+print("\n")
+
+#exercise asks for a for loop for each number
+for value in range(1,20,2):
+	print(value)
