c5cbc226abccb4ec938516fe12346acc28fe7057
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 23:25:51 +0100

    Solution for exercise try_4.4

---
+++ Chapter_4/try_4.4.py
@@ -0,0 +1,10 @@
+#Done by Carlos Amaral in 15/06/2020
+
+"""
+Make a list of the numbers from one to one million, and then
+use a for loop to print the numbers. (If the output is taking too long, stop it by
+pressing ctrl -C or by closing the output window.)
+"""
+
+for value in range(1,1000000):
+	print(value)
