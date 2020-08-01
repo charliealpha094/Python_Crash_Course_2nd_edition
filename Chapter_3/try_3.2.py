b02f8097a17aec707e4692e4742913ff108c6909
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 18:05:27 +0100

    Solution to exercise try_3.2

---
+++ Chapter_3/try_3.2.py
@@ -0,0 +1,22 @@
+#Done by Carlos Amaral on 12/06/2020
+
+"""
+Start with the list you used in Exercise 3-1, but instead of just
+printing each person’s name, print a message to them. The text of each mes-
+sage should be the same, but each message should be personalized with the
+person’s name.
+"""
+
+friends=['Rita', 'Catarina', 'Emilia', 'Patrícia']
+
+message= "Hello, " + friends[0].title() +"!!!"
+print(message)
+
+message= "Hello, " +friends[1].title() + "!!!"
+print(message)
+
+message= "Hello, " + friends[2].title() + "!!!"
+print(message)
+
+message= "Hello, " + friends[3].title() + "!!!"
+print (message)
