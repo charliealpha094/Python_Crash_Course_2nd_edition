86c53d9adc1a561ff19baac9be41202b24be5626
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 18:00:21 +0100

    Solution to exercise try_3.1.py

---
+++ Chapter_3/try_3.1.py
@@ -0,0 +1,13 @@
+#Done by Carlos Amaral in 12/06/2020
+
+"""
+Store the names of a few of your friends in a list called names . Print
+each person’s name by accessing each element in the list, one at a time.
+"""
+
+friends=['Rita', 'Catarina', 'Emilia', 'Patrícia']
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
