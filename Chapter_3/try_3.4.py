bbc241b5ceb1941d63bc30ae69adaf12c28c484c
charliealpha094 <carlosamaral94@gmail.com>
2020-08-01 18:48:22 +0100

    Solution to exercise try_3.4.py

---
+++ Chapter_3/try_3.4.py
@@ -0,0 +1,23 @@
+#Done by Carlos Amaral in 12/06/2020
+
+"""
+If you could invite anyone, living or deceased, to dinner, who
+would you invite? Make a list that includes at least three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting
+them to dinner.
+"""
+
+guests = ['Rita', 'Sofia', 'Patrícia']
+print(guests)
+
+message = "Dear " + guests[0].title() + \
+          " I would like to invite you to my dinner"
+print(message)
+
+message = "Dear " + guests[1].title() + \
+          " I would like to invite you to my dinner"
+print(message)
+
+message = "Dear " + guests[2].title() + \
+        " I would like to invite you to my dinner"
+print (message)
