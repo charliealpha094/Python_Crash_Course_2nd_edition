05ce3eabece6c64418b5bd21506fb99a18b42d3b
charliealpha094 <carlosamaral94@gmail.com>
2020-08-02 23:34:31 +0100

    Solution to try_5.11-Ordinal Numbers*

---
+++ Chapter_5/try_5.11.py
@@ -0,0 +1,25 @@
+#Done by Carlos Amaral in 20/06/2020
+
+"""
+Ordinal numbers indicate their position in a list, such
+as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+• Store the numbers 1 through 9 in a list.
+• Loop through the list.
+• Use an if - elif - else chain inside the loop to print the proper ordinal end-
+ing for each number. Your output should read "1st 2nd 3rd 4th 5th 6th
+7th 8th 9th" , and each result should be on a separate line.
+"""
+
+#Ordinal numbers
+
+ordinal_numbers = list(range(1,10))
+
+for ordinal_number in ordinal_numbers:
+    if ordinal_number == 1:
+        print("1st")
+    elif ordinal_number == 2:
+        print("2nd")
+    elif ordinal_number == 3:
+        print("3rd")
+    else:
+        print(str(ordinal_number) + "th")   #str function converts a specified value into a string
