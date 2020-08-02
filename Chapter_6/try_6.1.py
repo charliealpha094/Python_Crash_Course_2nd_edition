bad973988ffd1d24e908eb85a38e7d8f91dd77fb
charliealpha094 <carlosamaral94@gmail.com>
2020-08-02 23:57:40 +0100

    Solution to try_6.1-Person

---
+++ Chapter_6/try_6.1.py
@@ -0,0 +1,21 @@
+#Done by Carlos Amaral in 21/06/2020
+
+"""
+Use a dictionary to store information about a person you know.
+Store their first name, last name, age, and the city in which they live. You
+should have keys such as first_name , last_name , age , and city . Print each
+piece of information stored in your dictionary.
+"""
+
+#Person
+
+Patricia = {
+    'first_name': 'Ana',
+    'last_name': 'Soares',
+    'age': '33',
+    'city': 'Viseu'    
+}
+print(Patricia['first_name'])
+print(Patricia['last_name'])
+print(Patricia['age'])
+print(Patricia['city'])
