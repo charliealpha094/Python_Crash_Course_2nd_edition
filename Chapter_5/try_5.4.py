f425ea4dbe5b82e96273620cde0c37d58ce017c2
charliealpha094 <carlosamaral94@gmail.com>
2020-08-02 21:46:13 +0100

    Solution to exercise try_5.4-Alien Colors #2

---
+++ Chapter_5/try_5.4.py
@@ -0,0 +1,20 @@
+#Done by Carlos Amaral in 18/06/2020
+
+
+#Alien Colors 2
+
+#(This version runs the else block)
+alien_color = 'blue'
+if alien_color == 'green':
+    print("Congratulations! You've just earned 5 points!")
+else:
+    print("Congratulations! You've just earned 10 points!")
+
+print("\n")
+
+#(This version runs the if block)
+alien_color = 'green'
+if alien_color == 'green':
+    print("Congratulations! You've just earned 5 points!")
+else:
+    print("Congratulations! You've just earned 10 points")
