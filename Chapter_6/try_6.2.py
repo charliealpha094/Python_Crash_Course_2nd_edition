03b2c11ea434e1846c594c30287b9542186fe4ac
charliealpha094 <carlosamaral94@gmail.com>
2020-08-03 00:04:10 +0100

    Solution to try_6.2-Favourite numbers

---
+++ Chapter_6/try_6.2.py
@@ -0,0 +1,34 @@
+#Done by Carlos Amaral in 21/06/2020
+
+"""
+Use a dictionary to store people’s favorite numbers.
+Think of five names, and use them as keys in your dictionary. Think of a favorite
+number for each person, and store each as a value in your dictionary. Print
+each person’s name and their favorite number. For even more fun, poll a few
+friends and get some actual data for your program.
+"""
+
+#Favourite numbers
+
+favourite_numbers = {
+    'Carlos': '17',
+    'Patricia': '12',
+    'Rita': '23',
+    'Sofia': '2',
+    'Eva': '15'
+    }
+    
+number = favourite_numbers['Carlos']
+print(f"Carlos' favourite number is {number}.")
+
+number = favourite_numbers['Patricia']
+print(f"Patricia's favourite number is {number}.")
+
+number = favourite_numbers['Rita']
+print(f"Rita's favourite number is {number}.")
+
+number = favourite_numbers['Sofia']
+print(f"Sofia's favourite number is {number}.")
+
+number = favourite_numbers['Eva']
+print(f"Eva's favourite number is {number}.")
