3ada5e5a1e6a2f34c17ad0d70f28bb7503f30a07
charliealpha094 <carlosamaral94@gmail.com>
2020-08-02 22:24:05 +0100

    Solution to try_5.7-Favourite Fruit

---
+++ Chapter_5/try_5.7.py
@@ -0,0 +1,24 @@
+#Done by Carlos Amaral in 18/06/2020
+
+"""
+Make a list of your favorite fruits, and then write a series of
+independent if statements that check for certain fruits in your list.
+• Make a list of your three favorite fruits and call it favorite_fruits .
+• Write five if statements. Each should check whether a certain kind of fruit
+is in your list. If the fruit is in your list, the if block should print a statement,
+such as You really like bananas!
+"""
+
+#Favourite fruit
+favourite_fruits = ['kiwifruit', 'banana', 'apple', 'pineaple', 'strawberry']
+
+if 'kiwifruit' in favourite_fruits:
+    print("You really like kiwifruits!")
+if 'banana' in favourite_fruits:
+    print("You really like bananas!")
+if 'apple' in favourite_fruits:
+    print("You really like apples!")
+if 'pineaple' in favourite_fruits:
+    print("You really like pineaples!")
+if 'strawberry' in favourite_fruits:
+    print("You really like strawberries!")
