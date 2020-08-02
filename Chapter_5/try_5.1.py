848d8f06ad695fd529e59a1a473a736e49026fc6
charliealpha094 <carlosamaral94@gmail.com>
2020-08-02 18:29:52 +0100

    Solution to exercise try_5.1 - Conditional tests

---
+++ Chapter_5/try_5.1.py
@@ -0,0 +1,43 @@
+#Done by Carlos Amaral in 17/06/2020
+
+"""
+Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+• Look closely at your results, and make sure you understand why each line
+evaluates to True or False .
+• Create at least ten tests. Have at least five tests evaluate to True and
+another five tests evaluate to False .
+"""
+
+car='subaru'
+print("Is car == 'subaru'? I predict True")
+print (car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False")
+print (car == 'audi')
+
+
+
+
+food='pizza'
+print("\nIs food == 'pizza'? I predict True")
+print(food =='pizza')
+
+print("\nIs food == 'salomon'? I predict False")
+print(food == 'salomon')
+
+
+
+
+shoes='nike'
+print("\nIs shoes == 'nike'? I predict True")
+print(shoes == 'nike')
+
+print("\nIs shoes == 'adidas'? I predict False")
+print(shoes == 'adidas')
