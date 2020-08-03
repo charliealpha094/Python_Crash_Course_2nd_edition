818baa5e33b7d39f955a646dac1f8ddfc4b9c165
charliealpha094 <carlosamaral94@gmail.com>
2020-08-03 11:36:49 +0100

    Solution to try_7.1-Rental Car

---
+++ Chapter_7/try_7.1.py
@@ -0,0 +1,14 @@
+#Done by Carlos Amaral in 23/06/2020
+
+"""
+Write a program that asks the user what kind of rental car they
+would like. Print a message about that car, such as “Let me see if I can find you
+a Subaru.”
+"""
+
+#Rental Car
+
+car = input("Which rental car would you like?")
+print(car)
+
+print(f"Let me see if I can find you a {car}!")
