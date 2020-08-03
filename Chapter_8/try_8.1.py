784b53a8dfee8e560a54be61e80bbcd6f5bdf170
charliealpha094 <carlosamaral94@gmail.com>
2020-08-03 14:50:59 +0100

    Solution to try_8.1-Message

---
+++ Chapter_8/try_8.1.py
@@ -0,0 +1,14 @@
+#Done by Carlos Amaral in 28/06/2020
+
+"""
+Write a function called display_message() that prints one sen-
+tence telling everyone what you are learning about in this chapter. Call the
+function, and make sure the message displays correctly.
+"""
+
+#Message
+def display_message():
+    """Display a simple message"""
+    print("In this chapter, I'm learning about python functions.")
+
+display_message()
