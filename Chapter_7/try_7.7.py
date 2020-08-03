65552d5208fc4f12228c9faf4ae76e721059b809
charliealpha094 <carlosamaral94@gmail.com>
2020-08-03 12:23:28 +0100

    Solution to try_7.7-Infinity

---
+++ Chapter_7/try_7.7.py
@@ -0,0 +1,13 @@
+#Done by Carlos Amaral in 25/06/2020
+
+"""
+Write a loop that never ends, and run it. (To end the loop, press
+ctrl -C or close the window displaying the output.)
+"""
+
+#Infinity
+
+x = 1
+
+while x < 5:
+    print (x)
