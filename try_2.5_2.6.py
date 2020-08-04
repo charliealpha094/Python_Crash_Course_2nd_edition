c9ef6bdf7d2839a3a009dae41da3d3dbeb28f5ff
Carlos Amaral <carlosamaral94@gmail.com>
2020-08-04 14:27:37 +0100

    Delete try_2.5_2.6.py

--- try_2.5_2.6.py
+++
@@ -1,9 +0,0 @@
-#Author: Carlos Amaral
-#Date 11/06/2020
-
-print('Someone once said, "A sorte protege alguns audazes!"')
-
-
-famous_person="Unknown"
-message=f'{famous_person.title()},once said, "A sorte protege alguns audazes!"'
-print(message)
