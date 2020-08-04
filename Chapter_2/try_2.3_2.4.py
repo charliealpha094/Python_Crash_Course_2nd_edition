15f5eb0f4592880ecf3b41fe679dfe92d3cee324
charliealpha094 <carlosamaral94@gmail.com>
2020-08-04 14:05:10 +0100

    Solution to try_2.3 and try_2.4

---
+++ Chapter_2/try_2.3_2.4.py
@@ -0,0 +1,12 @@
+#Done by Carlos Amaral 2020-06-11
+
+name = "Carlos"
+full_name = f"{name}"
+print(f"Hello, {full_name.title()}, would you like to learn some Python today?")
+
+
+
+name = "Patrícia"
+print(name.upper())
+print(name.lower())
+print(name.title())
