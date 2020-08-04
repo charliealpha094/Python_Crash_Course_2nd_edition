a141af0112907068b45d3cc55349fd0fd697c894
Carlos Amaral <carlosamaral94@gmail.com>
2020-08-04 14:54:32 +0100

    Update try_3.1.py

--- Chapter_3/try_3.1.py
+++ Chapter_3/try_3.1.py
@@ -5,7 +5,7 @@ Store the names of a few of your friends in a list called names . Print
 each person’s name by accessing each element in the list, one at a time.
 """
 
-friends=['Rita', 'Catarina', 'Emilia', 'Patrícia']
+friends = ['Rita', 'Catarina', 'Emilia', 'Patrícia']
 
 print(friends[0])
 print(friends[1])
